60d9c5e353fe152ec087cc53bdf67190abd0b2b0
Pierre Leroux <p.leroux@gmx.com>
2021-03-03 12:51:58 +0100

    gactions pull sdk

---
+++ sdk/webhooks/ActionsOnGoogleFulfillment/index.js
@@ -0,0 +1,16 @@
+const { conversation } = require('@assistant/conversation');
+const functions = require('firebase-functions');
+
+const app = conversation();
+
+app.handle('<your handler name>', conv => {
+  // Implement your code here
+  conv.add("<your prompt here>");
+});
+
+exports.ActionsOnGoogleFulfillment = functions.https.onRequest(app);
+
+
+// NOT USED
+
+// SEE ../../webhooks
