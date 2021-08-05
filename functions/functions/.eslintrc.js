71ae2400350755c8e93e6fb5b0035893e7c979f8
Pierre Leroux <p.leroux@gmx.com>
2021-08-05 15:20:52 +0200

    publish function

--- functions/functions/.eslintrc.js
+++ functions/functions/.eslintrc.js
@@ -4,11 +4,15 @@ module.exports = {
     es6: true,
     node: true,
   },
+  "parserOptions": {
+    "ecmaVersion": 2017
+  },
   extends: [
     "eslint:recommended",
-    "google",
+    // "google",
   ],
   rules: {
     quotes: ["error", "double"],
+    "no-unused-vars": "off"
   },
 };
--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,8 +1,8 @@
-const { conversation, Media } = require('@assistant/conversation');
-const functions = require('firebase-functions');
- 
-const { OpdsFetcher } = require('opds-fetcher-parser');
-const { ok } = require('assert');
+const {conversation, Media} = require("@assistant/conversation");
+const functions = require("firebase-functions");
+
+const {OpdsFetcher} = require("opds-fetcher-parser");
+const {ok} = require("assert");
 
 function isValidHttpUrl(string) {
   let url;
@@ -18,30 +18,28 @@ function isValidHttpUrl(string) {
 
 const app = conversation();
 
-app.handle('cancel', conv => {
+app.handle("cancel", (conv) => {
   // Implement your code here
   conv.add("cancel");
 });
 
-app.handle('reprendre_mon_livre_lvl2', conv => {
-  
+app.handle("reprendre_mon_livre_lvl2", (conv) => {
+
   // void
-  
+
   // set the context storage
   // then the scene start the player
 });
 
-app.handle('ecouter_livre_audio_lvl2', conv => {
-  
+app.handle("ecouter_livre_audio_lvl2", (conv) => {
   // void
 
   console.log("écouter_livre_audio_lvl2");
 });
 
 async function getPubsFromFeed(query) {
-
   ok(typeof query === "string", "query not defined");
-  const url = SEARCH_URL.replace('{query}', encodeURIComponent(query));
+  const url = SEARCH_URL.replace("{query}", encodeURIComponent(query));
   console.log(url);
 
   const opds = new OpdsFetcher();
@@ -49,26 +47,26 @@ async function getPubsFromFeed(query) {
 
   ok(Array.isArray(feed.publications), "no publications");
   const list = feed.publications
-    .filter(({ openAccessLinks: l }) /*: l is IOpdsLinkView[]*/ => {
-      return (
-        Array.isArray(l) &&
+      .filter(({openAccessLinks: l}) /* : l is IOpdsLinkView[]*/ => {
+        return (
+          Array.isArray(l) &&
         l[0] &&
         isValidHttpUrl(l[0].url)
-      );
-    })
-    .slice(0, 5)
-    .map(({ title, authors, openAccessLinks }) => ({
-      title: title,
-      author: Array.isArray(authors) ? authors[0].name : '',
-      webpuburl: openAccessLinks[0].url
-    }));
+        );
+      })
+      .slice(0, 5)
+      .map(({title, authors, openAccessLinks}) => ({
+        title: title,
+        author: Array.isArray(authors) ? authors[0].name : "",
+        webpuburl: openAccessLinks[0].url,
+      }));
 
   return list;
 }
 
 
 const SEARCH_URL = "https://europe-west1-audiobooks-a6348.cloudfunctions.net/indexer?url=https://storage.googleapis.com/audiobook_edrlab/navigation/all.json&query={query}";
-app.handle('i_want_to_listen', async conv => {
+app.handle("i_want_to_listen", async (conv) => {
   // void
 
   console.log("i_want_to_listen START");
@@ -77,7 +75,7 @@ app.handle('i_want_to_listen', async conv => {
 
   try {
     query = conv.intent.params.query.resolved;
-  } catch {
+  } catch (_) {
     // ignore
   }
 
@@ -93,13 +91,12 @@ app.handle('i_want_to_listen', async conv => {
 
     conv.add(`Il y a ${length} publications :\nPour choisir une publication dite son numéro`);
 
-    let text = '';
-    list.map(({ title, author }, i) => {
-      text += `numero ${i + 1} : ${title} ${author ? `de ${author}` : ''}\n`
+    let text = "";
+    list.map(({title, author}, i) => {
+      text += `numero ${i + 1} : ${title} ${author ? `de ${author}` : ""}\n`;
     });
 
     conv.add(text);
-
   } else if (length === 1) {
     conv.scene.next.name = "player";
 
@@ -113,8 +110,7 @@ app.handle('i_want_to_listen', async conv => {
   // slot available for research
 });
 
-app.handle('select_publication_number', async conv => {
-
+app.handle("select_publication_number", async (conv) => {
   console.log("select_publication_number START");
 
   const number = conv.intent.params.number.resolved;
@@ -123,17 +119,16 @@ app.handle('select_publication_number', async conv => {
   const list = await getPubsFromFeed(conv.session.params.query);
   const pub = list[number - 1];
   if (pub) {
-
     console.log("PUB: ", pub);
 
     const url = pub.webpuburl;
 
-    if (!conv.user.params.player)
+    if (!conv.user.params.player) {
       conv.user.params.player = {};
+    }
 
     const history = conv.user.params.player[url];
     if (!history) {
-
       conv.user.params.player_startIndex = 0;
       conv.user.params.player_startTime = 0;
     } else {
@@ -141,7 +136,6 @@ app.handle('select_publication_number', async conv => {
       conv.user.params.player_startTime = history.t;
     }
     conv.user.params.player_url = url;
-
   } else {
     console.log("NO PUBS found !!");
     conv.scene.next.name = "search";
@@ -151,9 +145,7 @@ app.handle('select_publication_number', async conv => {
 });
 
 
-
-app.handle('player', async conv => {
-
+app.handle("player", async (conv) => {
   const url = conv.user.params.player_url;
   ok(url, "url not defined");
   ok(isValidHttpUrl(url), "url not valid " + url);
@@ -164,43 +156,43 @@ app.handle('player', async conv => {
 
   const startIndexRaw = conv.user.params.player_startIndex;
   const startIndex =
-    typeof startIndexRaw === 'number' &&
+    typeof startIndexRaw === "number" &&
       startIndexRaw <= webpub.readingOrders.length ? startIndexRaw : 0;
 
   const startTimeRaw = conv.user.params.player_startTime;
   const startTime =
-    typeof startTimeRaw === 'number' &&
+    typeof startTimeRaw === "number" &&
       startTimeRaw <= (webpub.readingOrders[startIndex].duration || Infinity) ? startTimeRaw : 0;
 
   const mediaObjects = webpub.readingOrders
-    .map((v, i) => ({
-      name: `${webpub.title || ''} - ${i + 1}`,
-      url: v.url,
-      image: {
-        large: {
-          alt: webpub.title,
-          url: webpub.cover || '',
+      .map((v, i) => ({
+        name: `${webpub.title || ""} - ${i + 1}`,
+        url: v.url,
+        image: {
+          large: {
+            alt: webpub.title,
+            url: webpub.cover || "",
+          },
         },
-      },
-    })).slice(startIndex);
+      })).slice(startIndex);
 
   console.log("Media list");
   console.log(mediaObjects);
 
   conv.add(
-    new Media({
-      mediaObjects: mediaObjects,
-      mediaType: 'AUDIO',
-      optionalMediaControls: ['PAUSED', 'STOPPED'],
-      startOffset: `${startTime}s`,
-    })
+      new Media({
+        mediaObjects: mediaObjects,
+        mediaType: "AUDIO",
+        optionalMediaControls: ["PAUSED", "STOPPED"],
+        startOffset: `${startTime}s`,
+      }),
   );
 });
 
 
-//////////////////////////
+// ////////////////////////
 // Media PLAYET CONTEXT //
-//////////////////////////
+// ////////////////////////
 
 function persistMediaPlayer(conv) {
   if (conv.request.context) {
@@ -213,8 +205,9 @@ function persistMediaPlayer(conv) {
     conv.user.params.player_startIndex = index;
     conv.user.params.player_startTime = progress;
 
-    if (!conv.user.params.player)
+    if (!conv.user.params.player) {
       conv.user.params.player = {};
+    }
 
     conv.user.params.player[url] = {
       i: index,
@@ -227,8 +220,7 @@ function persistMediaPlayer(conv) {
   }
 }
 
-app.handle('reprendre_la_lecture', (conv) => {
-
+app.handle("reprendre_la_lecture", (conv) => {
   persistMediaPlayer(conv);
 
   // // Acknowledge pause/stop
@@ -236,17 +228,10 @@ app.handle('reprendre_la_lecture', (conv) => {
   //   mediaType: 'MEDIA_STATUS_ACK'
   // }));
 
-  // -> reprendre la lecture
-  // -> temps restant : lecture bloqué
-  // -> reprendre la lecture : lecture bloqué également
-  // tester avec test online Actrionbuilder
-
   conv.scene.next.name = "player";
-
 });
 
-app.handle('remaining_time', async conv => {
-
+app.handle("remaining_time", async (conv) => {
   persistMediaPlayer(conv);
 
   const url = conv.user.params.player_url;
@@ -263,8 +248,9 @@ app.handle('remaining_time', async conv => {
   let minutes = 0;
   if (Array.isArray(webpub.readingOrders)) {
     let remainingTime = 0;
-    for (let i = index + 1; i < webpub.readingOrders.length; i += 1)
+    for (let i = index + 1; i < webpub.readingOrders.length; i += 1) {
       remainingTime += webpub.readingOrders[i].duration || 0;
+    }
     const pos = (v) => (v < 0 ? 0 : v);
     remainingTime += pos((webpub.readingOrders[index].duration || 0) - time);
 
@@ -289,35 +275,34 @@ app.handle('remaining_time', async conv => {
   conv.scene.next.name = "player";
 });
 
-//////////////////////////
+// ////////////////////////
 // Media PLAYET CONTEXT //
-//////////////////////////
+// ////////////////////////
 
 // Media status
-app.handle('media_status', (conv) => {
-
+app.handle("media_status", (conv) => {
   console.log("MediaStatus START");
   const mediaStatus = conv.intent.params.MEDIA_STATUS.resolved;
   console.log("MediaStatus : ", mediaStatus);
   switch (mediaStatus) {
-    case 'FINISHED':
+    case "FINISHED":
 
       // void
       break;
-    case 'FAILED':
+    case "FAILED":
 
       // void
       break;
-    case 'PAUSED':
-    case 'STOPPED':
+    case "PAUSED":
+    case "STOPPED":
       persistMediaPlayer(conv);
       // Acknowledge pause/stop
       conv.add(new Media({
-        mediaType: 'MEDIA_STATUS_ACK'
+        mediaType: "MEDIA_STATUS_ACK",
       }));
       break;
     default:
-      conv.add('media status incorrect');
+      conv.add("media status incorrect");
   }
 
   console.log("MediaStatus END");
@@ -327,7 +312,6 @@ app.handle('media_status', (conv) => {
 // catch(catcher: ExceptionHandler<TConversation>): ConversationV3App<TConversation>
 // ExceptionHandler(conv: TConversation, error: Error): any
 app.catch((conv, error) => {
-
   conv.add("une erreur s'est produite");
 
   console.log("ERROR");
